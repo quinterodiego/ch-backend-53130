de9e5226a944f7c23ff5d9f904f507fbd57b3908
quinterodiego <d86webs@gmail.com>
2024-03-27 11:57:38 -0300

    primera pre entrega

--- src/routers/products.routes.js
+++ src/routers/products.routes.js
@@ -6,62 +6,99 @@ const productsRouter = Router()
 const manager = new ProductManager('./src/data/products.json')
 
 productsRouter.get('/', async (req, res) => {
+  try {
+    let result = []
     const products = await manager.getProducts()
     const { limit } = req.query
     if(limit) {
-        const productsLimit = products.splice(0, parseInt(limit))
-        res.status(200).send({ 
-            "status": "success",
-            "payload": productsLimit 
-        })
+      result = products.splice(0, parseInt(limit))
     } else {
-        res.status(200).send({ 
-            "status": "success",
-            "payload": products 
-        })
+      result = products
     }
+    res.status(200).json({ 
+      "status": "success",
+      "result": result 
+  })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ 
+      "status": "error",
+      "message": "No se pudieron obtener los productos" 
+  })
+  }
+   
 })
 
 productsRouter.get('/:pid', async (req, res) => {
+  try {
     const id = parseInt(req.params.pid)
     const product = await manager.getProductById(id)
-    if(product) {
-        res.status(200).send({ 
-            "status": "success",
-            "payload": product
-        })
-    } else {
-        res.send({ error: 'Producto no encontrado'})
-    }
+    res.status(200).json({ 
+      "status": "success",
+      "result": product
+  })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ 
+      "status": "error",
+      "message": "No se pudo obtener el producto" 
+  })
+  }
 })
 
 productsRouter.post('/', productValidator, async (req, res) => {
+  try {
     const product = req.body
     const resp = await manager.addProduct(product)
-    res.status(200).send({
+    res.status(201).send({
         "status": "success",
-        "message": resp
+        "result": resp
     })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ 
+      "status": "error",
+      "message": "No se pudo crear el producto" 
+  })
+  }
+   
 })
 
 productsRouter.put('/:pid', async (req, res) => {
+  try {
     const id = parseInt(req.params.pid)
     const updates = req.body
-    console.log(updates)
     const resp = await manager.updateProduct(id, updates)
     res.status(201).send({
         "status": "success",
-        "message": resp
+        "result": resp
     })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ 
+      "status": "error",
+      "message": "No se pudo actualizar el producto" 
+  })
+  }
+   
 })
 
 productsRouter.delete('/:pid', async (req, res) => {
+  try {
     const id = parseInt(req.params.pid)
     const resp = await manager.deleteProduct(id)
     res.status(200).send({
         "status": "success",
-        "message": resp
+        "result": resp
     })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ 
+      "status": "error",
+      "message": "No se pudo eliminar el producto" 
+  })
+  }
+   
 })
 
 export default productsRouter
